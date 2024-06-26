6c72a48f9efb91f954485b174cd1aaebfb2c2c2a
aghahoseini <aghahoseini.savalan@gmail.com>
2024-06-26 08:50:38 +0330

    Create a.sql

---
+++ mysql_scripts/a.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS bash;
+
+CREATE TABLE IF NOT EXISTS bash.a1_logs_20240617  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
+INSERT INTO  bash.a1_logs_20240617 ( fname,lname ) VALUES ("eee" , "last name"),("fff","last name");
+
+
+CREATE TABLE IF NOT EXISTS bash.a1_logs_20240618  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
+INSERT INTO  bash.a1_logs_20240618 ( fname,lname ) VALUES ("ggg" , "last name"),("hhh","last name");
+
+CREATE TABLE IF NOT EXISTS bash.a1_logs_20240619  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
+INSERT INTO  bash.a1_logs_20240619 ( fname,lname ) VALUES ("III" , "last name"),("JJJ","last name");
+
+CREATE TABLE IF NOT EXISTS bash.a1_logs_20240620  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
+INSERT INTO  bash.a1_logs_20240620 ( fname,lname ) VALUES ("aaa" , "last name"),("bbb","last name");
+
+CREATE TABLE IF NOT EXISTS bash.a1_logs_20240621  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
+INSERT INTO  bash.a1_logs_20240621 ( fname,lname ) VALUES ("ccc" , "last name"),("ddd","last name");
