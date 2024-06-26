2b64c4d849da754507563d2861ffdc5f9b81ff62
aghahoseini <aghahoseini.savalan@gmail.com>
2024-06-26 08:55:37 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -16,17 +16,17 @@ mkdir -p $backup_save_path
 
 
 #now table_names is array
-table_names=(`mysql -u $db_user -P $db_port -p$db_password -h $db_host -e "USE bash;SHOW TABLES LIKE 'a1_logs_%';" | grep a1 | grep -v bash`)#2020y
+table_names=(`mysql -u $db_user -P $db_port -p$db_password -h $db_host -e "USE bash;SHOW TABLES LIKE 'a1_logs_%';" | grep a1 | grep -v bash`)
 #echo ${table_names[@]};
 
 
 #calculate 7 days ago time
-#output of above command without pipe --->  line1: CURDATE() - INTERVAL 7 DAY  line 2:2024-06-18#2020y
+#output of above command without pipe --->  line1: CURDATE() - INTERVAL 7 DAY  line 2:2024-06-18
 #output of above command after pipe 2024-06-18
 seven_days_ago=`mysql -u $db_user -P $db_port -p$db_password -h $db_host -e "SELECT CURDATE() - INTERVAL 7 DAY;" | sed -n '2p'` 
 
-#convert 2024-06-18 to 20240618
-#seven_days_ago=`date -d $seven_days_ago +'%y%m%d'`#2020y
+#convert 24-06-18 to 240618
+#seven_days_ago=`date -d $seven_days_ago +'%y%m%d'`
 seven_days_ago=`date -d $seven_days_ago +'%Y%m%d'`
 
 
@@ -34,7 +34,7 @@ seven_days_ago=`date -d $seven_days_ago +'%Y%m%d'`
 
 for table in "${table_names[@]}"
 do
-    #convert a1_logs_20240527 to 20240527
+    #convert a1_logs_240527 to 240527
     date_section_in_table_name=`echo $table | awk -F"_" '{ print $3 }'`
 
     backup_file_name=$table".sql.gz"
@@ -48,3 +48,5 @@ do
     fi
 
 done
+mysql -u $db_user -P $db_port -p$db_password -h $db_host -e "use $db_name; show tables;"
+ls -l /home/backups/    
