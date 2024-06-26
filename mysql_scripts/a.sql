525b78b857a6e3ba9e182b548dbb97bb77598341
aghahoseini <aghahoseini.savalan@gmail.com>
2024-06-26 09:24:44 +0330

    Update a.sql

--- mysql_scripts/a.sql
+++ mysql_scripts/a.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS bash;
 
 CREATE TABLE IF NOT EXISTS bash.a1_logs_20240617  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
-INSERT INTO  bash.a1_logs_20240617 ( fname,lname ) VALUES ("eee" , "last name"),("fff","last name");
+INSERT INTO  bash.a1_logs_20240617 ( fname,lname ) VALUES ("17 000" , "last name"),("17 111","last name");
 
 
 CREATE TABLE IF NOT EXISTS bash.a1_logs_20240618  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
-INSERT INTO  bash.a1_logs_20240618 ( fname,lname ) VALUES ("ggg" , "last name"),("hhh","last name");
+INSERT INTO  bash.a1_logs_20240618 ( fname,lname ) VALUES ("18 000" , "last name"),("18 111","last name");
 
 CREATE TABLE IF NOT EXISTS bash.a1_logs_20240619  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
-INSERT INTO  bash.a1_logs_20240619 ( fname,lname ) VALUES ("III" , "last name"),("JJJ","last name");
+INSERT INTO  bash.a1_logs_20240619 ( fname,lname ) VALUES ("19 000" , "last name"),("19 111","last name");
 
 CREATE TABLE IF NOT EXISTS bash.a1_logs_20240620  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
-INSERT INTO  bash.a1_logs_20240620 ( fname,lname ) VALUES ("aaa" , "last name"),("bbb","last name");
+INSERT INTO  bash.a1_logs_20240620 ( fname,lname ) VALUES ("20 000" , "last name"),("20 111","last name");
 
 CREATE TABLE IF NOT EXISTS bash.a1_logs_20240621  (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(30) NOT NULL , lname VARCHAR(30) NOT NULL);
-INSERT INTO  bash.a1_logs_20240621 ( fname,lname ) VALUES ("ccc" , "last name"),("ddd","last name");
+INSERT INTO  bash.a1_logs_20240621 ( fname,lname ) VALUES ("21 000" , "last name"),("21 111","last name");
